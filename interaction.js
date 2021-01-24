6ea5fc1ddd18d9576ceae9fe943ccafa45dba14e
tennisplayer106 <76907264+tennisplayer106@users.noreply.github.com>
2021-01-24 20:35:42 +0530

    Add files via upload

---
+++ interaction.js
@@ -0,0 +1,20 @@
+function teleportation(){
+    window.open( "What Is SDG 7.html","_blank");
+}
+
+function teleportation1(){
+    window.open( "Causes and Effects.html","_blank");
+}
+
+function teleportation2(){
+    window.open( "Solutions.html","_blank");
+}
+
+function teleportation3(){
+    window.open( "Citations.html","_blank");
+}
+
+function teleportation4(){
+    window.open( "Feedback.html","_blank");
+}
+
